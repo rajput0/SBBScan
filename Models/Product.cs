13c167998ce2d3291cd9f302791ed59e032b087a
aaron <aaron@DESKTOP-3CVETLC>
2021-07-31 15:28:56 -0400

    Add project files.

---
+++ Models/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SBBScan.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public int AvailableQuantity { get; set; }
+        public DateTime LastOrdered { get; set; }
+        public DateTime OrderReminder { get; set; }
+        public int TotalSoldTillMonth { get; set; }
+        public int TotalSoldToday { get; set; }
+        public string Image { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public double Discount { get; set; }
+        
+
+    }
+}
