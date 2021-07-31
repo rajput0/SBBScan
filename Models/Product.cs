769c5109a40cb31906da11ce56a8bca36d466192
Raj <rajput0@sheridancollege.ca>
2021-07-31 15:39:41 -0400

    test username and email

--- Models/Product.cs
+++ Models/Product.cs
@@ -8,16 +8,16 @@ namespace SBBScan.Models
     public class Product
     {
         public int Id { get; set; }
-        public int AvailableQuantity { get; set; }
+        //public int AvailableQuantity { get; set; }
         public DateTime LastOrdered { get; set; }
         public DateTime OrderReminder { get; set; }
-        public int TotalSoldTillMonth { get; set; }
+        //public int TotalSoldTillMonth { get; set; }
         public int TotalSoldToday { get; set; }
-        public string Image { get; set; }
+        //public string Image { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public double Price { get; set; }
-        public double Discount { get; set; }
+        //public double Discount { get; set; }
         
 
     }
